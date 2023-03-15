a5b57b61645d942a15ec8111d24763d38be1fe6a
JunSeong_Park <ttotto0608@naver.com>
2023-03-15 13:35:35 +0900

    feat : adding Role Repository and implementation UserService

--- src/main/java/com/example/userservice/domain/RoleRepository.java
+++ src/main/java/com/example/userservice/domain/RoleRepository.java
@@ -2,8 +2,6 @@ package com.example.userservice.domain;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.Optional;
-
 public interface RoleRepository extends JpaRepository<Role, Long> {
-    Optional<Role> findByName(String name);
+    Role findByName(String name);
 }
