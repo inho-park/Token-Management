6268a306cec08b3087cb6da1a143a4a3a3c63c5b
JunSeong_Park <ttotto0608@naver.com>
2023-03-17 00:23:27 +0900

    feat : return to client with accessToken and refreshToken in responseBody

--- src/main/java/com/example/userservice/filter/CustomAuthenticationFilter.java
+++ src/main/java/com/example/userservice/filter/CustomAuthenticationFilter.java
@@ -1,10 +1,15 @@
 package com.example.userservice.filter;
 
-import lombok.RequiredArgsConstructor;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.http.MediaType;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
@@ -13,6 +18,12 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.stream.Collectors;
+import java.util.Map;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 @Log4j2
 public class CustomAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
@@ -44,7 +55,35 @@ public class CustomAuthenticationFilter extends UsernamePasswordAuthenticationFi
                                             FilterChain chain,
                                             // AuthenticationManager 가 인증한 로그인 결과
                                             // = attemptAuthentication() 의 반환 값
-                                            Authentication authResult) throws IOException, ServletException {
-        
+                                            Authentication authentication) throws IOException, ServletException {
+
+        // 사용자 로그인 성공
+        User user = (User)authentication.getPrincipal();
+        // signature 에 담길 시크릿키와 알고리즘 설정
+        Algorithm algorithm = Algorithm.HMAC256("secret".getBytes());
+
+        String access_token = JWT.create()
+                .withSubject(user.getUsername())
+                .withExpiresAt(new Date(System.currentTimeMillis() + 10*60*1000))
+                .withIssuer(request.getRequestURL().toString())
+                .withClaim("roles", user.getAuthorities()
+                        .stream()
+                        .map(GrantedAuthority::getAuthority).collect(Collectors.toList()))
+                .sign(algorithm);
+
+        String refresh_token = JWT.create()
+                .withSubject(user.getUsername())
+                .withExpiresAt(new Date(System.currentTimeMillis() + 10*60*1000))
+                .withIssuer(request.getRequestURL().toString())
+                .sign(algorithm);
+
+//        response.setHeader("access_token", access_token);
+//        response.setHeader("refresh_token", refresh_token);
+        // response header 에 담으면 보안에 취약하므로 바디에 담아서 보내기
+        Map<String, String> tokens = new HashMap<>();
+        tokens.put("access_token", access_token);
+        tokens.put("refresh_token", refresh_token);
+        response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+        new ObjectMapper().writeValue(response.getOutputStream(), tokens);
     }
 }
