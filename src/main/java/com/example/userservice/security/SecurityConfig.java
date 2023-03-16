88fd3e3d8530d19c099879b7def2c7b22da3dc6d
JunSeong_Park <ttotto0608@naver.com>
2023-03-17 00:23:50 +0900

    feat : set configure about endpoint

--- src/main/java/com/example/userservice/security/SecurityConfig.java
+++ src/main/java/com/example/userservice/security/SecurityConfig.java
@@ -4,6 +4,7 @@ import com.example.userservice.filter.CustomAuthenticationFilter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -18,28 +19,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 @RequiredArgsConstructor
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
     private final UserDetailsService userDetailsService;
-    private final BCryptPasswordEncoder encoder;
-
-    // authenticationManager 에서 사용할 UserDetailsService 객체를 인자로 받아
-    // DAO authenticationConfigurer 를 반환 받으면 그 안에 passwordEncoder 를 지정해줌
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.userDetailsService(userDetailsService).passwordEncoder(encoder);
-    }
-    
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        // csrf 금지
-        http.csrf().disable();
-        // http 프로토콜의 조건 중 하나인 STATELESS 무상태 구조
-        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
-        // ExpressionInterceptUrlRegistry 를 통해 각각 url 로의 접근 권한을 설정할 수 있게 해줌 
-        http.authorizeRequests()
-                // 특정한 Request url 외에 모든 url 에 어떤 권한이든 permit 해줌
-                .anyRequest().permitAll();
-        // filter 로 dispatcher servlet 에 접근하기 전에 먼저 authenticationManager 객체로 검사
-        http.addFilter(new CustomAuthenticationFilter(authenticationManagerBean()));
-    }
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
 
     // CustomFilter 에 사용할 AuthenticationManager 를 제공하기 위해 빈 생성
     @Bean
@@ -47,4 +27,35 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     public AuthenticationManager authenticationManagerBean() throws Exception {
         return super.authenticationManagerBean();
     }
+
+    // authenticationManager 에서 사용할 UserDetailsService 객체를 인자로 받아
+    // DAO authenticationConfigurer 를 반환 받으면 그 안에 passwordEncoder 를 지정해줌
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailsService).passwordEncoder(bCryptPasswordEncoder);
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        // CustomAuthenticationFilter 가 상속받는 UsernamePasswordAuthenticationFilter 는
+        // 기존에 로그인 처리 url 이 기본으로 잡혀있다
+        CustomAuthenticationFilter customAuthenticationFilter = new CustomAuthenticationFilter(authenticationManagerBean());
+        // 위와 같은 이유로 아래 코드를 통해 내가 원하는 로그인 엔드포인트를 지정해줄 수 있다
+        customAuthenticationFilter.setFilterProcessesUrl("/api/login");
+        // csrf 금지
+        http.csrf().disable();
+        // http 프로토콜의 조건 중 하나인 STATELESS 무상태 구조
+        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+        // ExpressionInterceptUrlRegistry 를 통해 각각 url 로의 접근 권한을 설정할 수 있게 해줌
+        http.authorizeRequests()
+                // 권한이나 인증 없이 permit 해줌
+                .antMatchers("/login").permitAll()
+                .antMatchers(HttpMethod.GET, "/api/user/**").hasAuthority("ROLE_USER")
+                .antMatchers(HttpMethod.POST, "/api/user/save/**").hasAuthority("ROLE_ADMIN")
+                // 인증(authenticated)되야만 접근 가능 antMatchers 로 지정한 엔드포인트 외에
+                .anyRequest().authenticated();
+        // filter 로 dispatcher servlet 에 접근하기 전에 먼저 authenticationManager 객체로 검사
+        http.addFilter(new CustomAuthenticationFilter(authenticationManagerBean()));
+    }
+
 }
